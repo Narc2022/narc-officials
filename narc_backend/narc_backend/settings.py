cf8a88a28cb02dea31ab0fedbc82778090fa5e0e
sachin <sachinnarofficials@gmail.com>
2025-06-30 20:30:49 +0530

    config of railway postgress db

--- narc_backend/narc_backend/settings.py
+++ narc_backend/narc_backend/settings.py
@@ -72,10 +72,15 @@ WSGI_APPLICATION = "narc_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "abc123",
+        "HOST": "containers-us-west-45.railway.app",
+        "PORT": "5432",
     }
 }
 
